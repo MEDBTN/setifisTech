23094acf949ebef5cc5a8281fed8a461e1c57f84
boutine_mohamed <boutine111@gmail.com>
2024-01-22 12:31:36 +0100

    scroll-to , animate on scroll

--- src/app/layout/body/body.component.ts
+++ src/app/layout/body/body.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit {
+  constructor(private scroller: ViewportScroller) {}
 
+  ngOnInit(): void {
+    AOS.init();
+    window.addEventListener('load', AOS.refresh);
+  }
+
+  isNavbarCollapsed = true;
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  scrollTo(elementId: string) {
+    this.scroller.scrollToAnchor(elementId);
+    this.toggleNavbar();
+  }
+  getStarted(elementId: string) {
+    this.scroller.scrollToAnchor(elementId);
+  }
 }
